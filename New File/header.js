68847a98b6a171d22f8961e2d54c60928d56e919
Gabriel Vinicius <gabrielviniciusdecs@gmail.com>
2025-07-03 11:25:39 -0300

    Add files via upload

---
+++ New File/header.js
@@ -0,0 +1,9 @@
+// Seleciona o botão do menu e o menu de navegação (alteração para funcionamento do menu responsivo)
+const menuToggle = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector('.nav-menu');
+// Adiciona/remover a classe 'active' para abrir/fechar o menu lateral (alteração para funcionamento do menu responsivo)
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('active');
+});
+
+
